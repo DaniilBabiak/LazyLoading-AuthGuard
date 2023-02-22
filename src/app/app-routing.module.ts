885ab0d61c7ff2b716e37314730e65a0d2e3d92e
Daniil Babiak <imperatorvkresle790@gmail.com>
2023-02-22 11:38:54 +0100

    Made routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./Components/login/login.component";
+import {ForgotPasswordComponent} from "./Components/forgot-password/forgot-password.component";
+import {NotFoundComponent} from "./Components/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'forgot-password', component:ForgotPasswordComponent},
+  {path: '', redirectTo:'/login', pathMatch: 'full'},
+  {path: '**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
