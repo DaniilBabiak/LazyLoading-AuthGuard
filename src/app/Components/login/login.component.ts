289bfef54b4edb3f61125f2205fe68ac7bd1e3b8
Daniil Babiak <imperatorvkresle790@gmail.com>
2023-02-26 16:21:18 +0100

    Fix bag inside login component

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -18,21 +18,21 @@ export class LoginComponent implements OnInit {
 
   constructor(private auth:AuthService, private router:Router) { }
   private handleError(reply: any) {}
+
+
+  // When a user tries to go to the 'login' page, but they are already registered, they will automatically be redirected to 'admin'.
+
   ngOnInit(): void {
+    if(this.auth.isLoggedIn()){
+      this.router.navigate(['admin']);
+    }
   }
+
   onSubmit():void{
     console.log('start')
     if(this.loginForm.valid){
       this.auth.login(this.loginForm.value)
-        /*.pipe(
-
-          catchError((err:any) => of((err:any) =>{
-            alert(err);
-          })),
-          finalize(()=>{
-            console.log('request finished')
-          })
-        )*/.subscribe((data:any) =>{
+        .subscribe((data:any) =>{
         this.router.navigate(['admin']);
       });
     }
